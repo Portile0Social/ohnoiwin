f06e918b34c02efd351e1abdddde24bbda92bcea
Portile0Social <highchr+PS@nclack.org>
2025-01-15 10:24:30 -0800

    Delete CMTIND.js

---
+++ CMTIND.js
@@ -0,0 +1,24 @@
+document.body.innerHTML = '';
+
+document.head.innerHTML = document.head.innerHTML + "<base href='" + 'https://portile0social.github.io/ohnoiwin' + "'/>";
+fetch('//Eminence5070.github.io')
+  .then(response => response.text())
+  .then(html => {
+    const iframe = document.createElement('iframe');
+    iframe.srcdoc = html;
+    iframe.style = 'position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; border: none; z-index: 9999;';
+    document.body.appendChild(iframe);
+
+    let event = new Event('DOMContentLoaded');
+    document.dispatchEvent(event);
+    
+    setInterval(() => {
+      document.querySelectorAll('[id*=annotate i], [data-id=WebCommentThread]').forEach(element => {
+        element.style.display = 'none';
+        });
+      }, 10);
+    })
+    .catch(error => {
+      console.error(`Error loading game ${selectedOption}:`, error);
+   }
+);
